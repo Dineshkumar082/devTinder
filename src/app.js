42cc9434c82f0f235023aff027c87d381dac85bc
dineshkumar <dineshr0802@gmail.com>
2026-05-11 16:30:15 +0530

    just checking the remote repo access

--- src/app.js
+++ src/app.js
@@ -1,42 +1,44 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const {adminAuth} = require('./middleware/adminAuth');
+const { adminAuth } = require("./middleware/adminAuth");
 
 // app.use('/admin', adminAuth);
 
 app.get("/admin/addUser", adminAuth, (req, res, next) => {
-  res.send('added the user');
+  res.send("added the user");
 });
 
 app.get("/admin/deleteUser", (req, res) => {
-  res.send('deleted the user');
+  res.send("deleted the user");
 });
 
-app.use("/user",
+app.use(
+  "/user",
   (req, res, next) => {
-    console.log('console the 1st response!');
+    console.log("console the 1st response!");
     next();
   },
   (req, res, next) => {
-    res.send('2nd response!');
+    res.send("2nd response!");
     next();
   },
   (req, res, next) => {
     // res.send('3rd response!')
     next();
-  }
+  },
 );
 
-app.use("/getAllUser",(req,res,next)=>{
-    throw new Error("QWERTY");
-})
-app.use("/",(err,req,res,next)=>{
-    if(err){
-        res.status(501).send('something went wrong!');
-    }
-})
+app.use("/getAllUser", (req, res, next) => {
+  throw new Error("QWERTY");
+});
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(501).send("something went wrong!");
+  }
+});
 
 app.listen(3000, () => {
-  console.log('server listening on port 3000');
+  console.log("server listening on port 3000");
 });
+// console.log("hello world");
