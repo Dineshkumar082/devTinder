d7e1833dfc9de98becd0adb994a5ff2416f55b88
dineshkumar <dineshr0802@gmail.com>
2026-05-18 20:51:04 +0530

    get single user and get all the user details for feed api

--- src/app.js
+++ src/app.js
@@ -6,6 +6,28 @@ const User = require("./model/user");
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 app.use(express.json());
+
+app.get("/user", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    console.log(user);
+
+    res.send(user);
+  } catch (err) {
+    console.log(err);
+
+    res.status(400).send("Something went wrong!", err);
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const user = await User.find({});
+    res.send(user);
+  } catch (err) {
+    res.send("Something went wrong!", err);
+  }
+});
 app.post("/signUp", async (req, res) => {
   try {
     console.log(req.body);
